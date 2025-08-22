625c7cfe86416895285b95857060060755fd756d
Richard Thornton <richt9662@gmail.com>
2025-08-22 11:26:48 +0100

    feature: store image data

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
 console.log("hello world");
+
+//Todo: I need to store my image data
+//I can use images stored locally or stored remotely
+//use images as objects in an array
+const images = [
+  {
+    url: "url1",
+    thumbnail: "thumbnail1",
+    altText: "altText1",
+  },
+  {
+    url: "url2",
+    thumbnail: "thumbnail2",
+    altText: "altText2",
+  },
+  {
+    url: "url3",
+    thumbnail: "thumbnail3",
+    altText: "altText3",
+  },
+];
+//Todo: I need to create my thumbnaail images
+
+//Todo: I need to create my large images
