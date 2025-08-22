7556236edcd10782b68773767e2d47ffc73fa9c5
Richard Thornton <richt9662@gmail.com>
2025-08-22 11:41:46 +0100

    feat: added tasks to my js so that I keep track of what i have and havent dont

--- app.js
+++ app.js
@@ -21,5 +21,15 @@ const images = [
   },
 ];
 //Todo: I need to create my thumbnaail images
+//funtion to create thumbnails{
+//select the DOM element(thumbnialContainer) to contain the thuumbnails
+//this is a repetative task --> loop through the aray (using the length property)
+//inside the loop
+// - create image element
+// -update the src and alt attributes to match those in your array parameters)
+// give each img a className (img.className)
+// - add an event listener to each image--> the event handler of this listener is the function you write to create large images
+// append the created images to the thumbnailContainer
 
+//}
 //Todo: I need to create my large images
